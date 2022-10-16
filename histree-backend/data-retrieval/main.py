26bac525fbd6c9bd771749c0ca3dba0be87a694d
Dyuti Chakraborty <dyutic.home@gmail.com>
2022-10-16 12:04:26 +0100

    HIS-12: add interface to distinguish between similar options

--- histree-backend/data-retrieval/main.py
+++ histree-backend/data-retrieval/main.py
@@ -1,6 +1,19 @@
 from wrapper import WikiNetwork
 
+
+seed = input("Name to Query: ")
+
 network = WikiNetwork()
-qid = network.retrieve_potential_seeds("Elizabeth II")[0][0]
-network.add_person_and_immediates(qid) # Elizabeth II
+matches = network.retrieve_potential_seeds(seed)
+
+print("Potential matches for Query: ")
+
+for i in range(0, len(matches)):
+    (_, label) = matches[i]
+    print(f"{i + 1}. {label}") 
+
+qid = int(input("Choose the option you wish to query: ")) - 1
+
+network.add_person_and_immediates(matches[qid][0]) 
+
 print(network.network)
