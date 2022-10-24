f709f7b893d0eeab74e406a4cfd8662ea0552688
Zaki Amin <za3290@ic.ac.uk>
2022-10-24 11:10:24 +0100

    HIS-26: set up credentials for Postgres database

--- deployment/histree.py
+++ deployment/histree.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config['DB_HOSTNAME'] = 'histree-db.internal'
+app.config['DB_USERNAME'] = 'postgres'
+app.config['DB_PASSWORD'] = 'hLnm3cGTjIejCox'
+app.config['DB_PROXY_PORT'] = 5432
+app.config['DB_POSTGRES_PORT'] = 5433
+app.config['DB_CREDENTIALS_LINK'] = 'postgres://postgres:hLnm3cGTjIejCox@histree-db.internal:5432'
 
 @app.route('/')
 @app.route('/<name>')
