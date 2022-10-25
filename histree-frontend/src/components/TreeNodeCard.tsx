b198abba728094c4249cadc81c08eefeea1f5d9f
Athena Yim <asy20@ic.ac.uk>
2022-10-25 15:46:43 +0100

    HIS-19: Use mui typography font and create modal skeleton

--- histree-frontend/src/components/TreeNodeCard.tsx
+++ histree-frontend/src/components/TreeNodeCard.tsx
@@ -1,11 +1,32 @@
-import React from "react";
+import { Drawer, Box, Typography } from "@mui/material";
+import React, { useState } from "react";
 import "./TreeNodeCard.scss";
 
 const TreeNodeCard = (props: { displayName: string }) => {
+  const [expanded, setExpanded] = useState(false);
+
   const { displayName } = props;
+
+  const expandWindow = () => {
+    setExpanded(!expanded);
+    console.log(`expanded: ${expanded}`);
+  };
+
   return (
-    <div className="treenodecard">
-      <h3>{displayName}</h3>
+    <div className="treenodecard" onClick={expandWindow}>
+      <Typography variant="body1">{displayName}</Typography>
+      {expanded && (
+        <Drawer
+          hideBackdrop
+          anchor="right"
+          open={expanded}
+          onClose={() => setExpanded(false)}
+        >
+          <Box className="treenodecard-modal">
+            <Typography variant="h4">{displayName}</Typography>
+          </Box>
+        </Drawer>
+      )}
     </div>
   );
 };
