3b15c7b852cc80448ba12c2e24d71acf2478b6a0
zaki-amin <zakiamin0402@gmail.com>
2022-11-01 14:28:48 +0000

    HIS-33A: host data retrieval on API, update .gitignore, write deployment script

--- deployment/histree.py
+++ deployment/histree.py
@@ -1,8 +1,12 @@
+
+
 from flask import Flask, render_template
 from typing import Dict
-import importlib
-histree_query = importlib.import_module(
-    "histree-backend.data_retrieval.histree_query")
+
+
+import sys
+sys.path.append('../histree-backend/data_retrieval/')
+from histree_query import HistreeQuery
 
 
 app = Flask(__name__)
@@ -11,12 +15,12 @@ app = Flask(__name__)
 # Pass in a name and return a dictionary of potential matches names to Wiki IDs
 @app.route('/find_matches/<name>')
 def find_matches(name: str):
-    matches: Dict[str, str] = histree_query.search_matching_names(name)
+    matches: Dict[str, str] = HistreeQuery.search_matching_names(name)
     return matches
 
 
 # Pass in a Wiki ID and return a json of all their relevant data
 @app.route('/person_info/<qid>')
 def person_info(qid):
-    tree_json: Dict[str, any] = histree_query.get_tree_from_id(qid)
+    tree_json: Dict[str, any] = HistreeQuery.get_tree_from_id(qid)
     return tree_json
