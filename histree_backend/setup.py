cd4cfd919bf279053d3e397a8d7700cb053c52c6
xLynna <lynna.xie@gmail.com>
2022-11-03 11:58:37 +0000

    remove histree_backend as explicit package

--- histree_backend/setup.py
+++ histree_backend/setup.py
@@ -2,5 +2,5 @@ from setuptools import setup, find_packages
 setup(
     name="histree_backend",
     version="2022",
-    packages=['histree_backend', *find_packages()],
+    packages=find_packages(),
 )
