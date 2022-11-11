1255675635514f548cbea2604b0885ea8a424041
Athena Yim <asy20@ic.ac.uk>
2022-11-11 14:46:35 +0000

    HIS-39: implement onclick handlers for expanding by branch

--- histree-frontend/src/components/TreeNode.tsx
+++ histree-frontend/src/components/TreeNode.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { setSelected, getSelected } from '../stores/base';
+import {
+  setSelected,
+  getSelected,
+  getRenderContent,
+  getVisible,
+  setVisible
+} from '../stores/base';
 import { NodeInfo } from '../models';
 import './TreeNode.scss';
 import { Handle, Position } from 'reactflow';
@@ -8,6 +14,8 @@ import { Handle, Position } from 'reactflow';
 const TreeNode = ({ data }: { data: NodeInfo }) => {
   const dispatch = useDispatch();
   const selected = useSelector(getSelected);
+  const renderContent = useSelector(getRenderContent);
+  const visible = useSelector(getVisible);
 
   const expandWindow = () => {
     dispatch(
@@ -22,13 +30,41 @@ const TreeNode = ({ data }: { data: NodeInfo }) => {
     console.log(selected);
   };
 
+  const handleExpandParents = (): void => {
+    if (renderContent.status === 'Success') {
+      const adjList = renderContent.content!.branches;
+      const newVis = { ...visible };
+
+      Object.keys(adjList).forEach((parentId) => {
+        if (adjList[parentId].includes(data.id)) {
+          newVis[parentId] = true;
+        }
+      });
+
+      dispatch(setVisible(newVis));
+    }
+  };
+
+  const handleExpandChildren = (): void => {
+    if (renderContent.status === 'Success') {
+      const adjList = renderContent.content!.branches;
+      const newVis = { ...visible };
+
+      adjList[data.id].forEach((childId) => {
+        newVis[childId] = true;
+      });
+
+      dispatch(setVisible(newVis));
+    }
+  };
+
   return (
     <>
       <Handle
         type="target"
         position={Position.Top}
         isConnectable
-        onClick={() => console.log('clicked top handle')}
+        onClick={handleExpandParents}
       />
       <div className="treenodecard" onClick={expandWindow}>
         <div className="treenodechild">{data.name}</div>
@@ -38,7 +74,7 @@ const TreeNode = ({ data }: { data: NodeInfo }) => {
         type="source"
         position={Position.Bottom}
         isConnectable
-        onClick={() => console.log('clicked bottom handle')}
+        onClick={handleExpandChildren}
       />
     </>
   );
