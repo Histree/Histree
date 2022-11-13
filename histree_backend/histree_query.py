554cba1e714c51d71f3941426459e638ff1278bc
matsagad <matsagad@gmail.com>
2022-11-13 08:38:27 +0000

    HIS-54: use query builder to search for names

--- histree_backend/histree_query.py
+++ histree_backend/histree_query.py
@@ -1,5 +1,6 @@
 import re
 from typing import Dict
+from data_retrieval.query.name_query import NameQueryBuilder
 from data_retrieval.wikitree.tree import WikiSeed, WikiTree
 from data_retrieval.wikitree_instance.familytree.seed import FamilySeed
 from qwikidata.sparql import return_sparql_query_results
@@ -11,8 +12,13 @@ class HistreeQuery:
         # standardises the capitalisation of query string to be consistent with Wikidata expectations
 
         prepos = {"the", "of", "in", "and"}
-        def isRomanNumeral(w): return bool(re.search(
-            r"^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$", w))
+
+        def isRomanNumeral(w):
+            return bool(
+                re.search(
+                    r"^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$", w
+                )
+            )
 
         words = name.split()
         for i in range(len(words)):
@@ -26,28 +32,20 @@ class HistreeQuery:
         return " ".join(words)
 
     @staticmethod
-    def search_matching_names(name: str, instance_of: str = "Q5", language: str = "en") -> Dict[int, Dict[str, str]]:
+    def search_matching_names(
+        name: str, instance: str = "Q5", language: str = "en"
+    ) -> Dict[int, Dict[str, str]]:
         # Check if name has been queried before in db and return if so
-        input = HistreeQuery.processQueryString(name)
+
         # Otherwise, query wikidata directly
-        query = f'''
-            SELECT ?item ?label ?description WHERE {{
-                SERVICE wikibase:mwapi {{
-                    bd:serviceParam wikibase:api "EntitySearch" .
-                    bd:serviceParam wikibase:endpoint "www.wikidata.org" .
-                    bd:serviceParam mwapi:search "{name}" .
-                    bd:serviceParam mwapi:language "{language}" .
-                    ?item wikibase:apiOutputItem mwapi:item .
-                    ?num wikibase:apiOrdinal true .
-                }}
-                ?item wdt:P31 wd:{instance_of};
-                        rdfs:label ?label;
-                        schema:description ?description.
-                FILTER(REGEX(?label, "^{name}"))
-                FILTER(LANG(?label) = "{language}")
-                FILTER(LANG(?description) = "{language}")
-            }} ORDER BY ASC(?num) LIMIT 10
-        '''
+        query = (
+            NameQueryBuilder()
+            .with_instance(instance)
+            .with_name(name)
+            .ordered_by("?num")
+            .with_limit(10)
+            .build()
+        )
 
         res = return_sparql_query_results(query)
 
@@ -55,31 +53,33 @@ class HistreeQuery:
 
         for (i, row) in enumerate(res["results"]["bindings"]):
             qid_label_map[i] = {
-                "id": row["item"]["value"].split('/')[-1],
+                "id": row["item"]["value"].split("/")[-1],
                 "label": row["label"]["value"],
-                "description": row["description"]["value"]
+                "description": row["description"]["value"],
             }
 
         return qid_label_map
 
     @staticmethod
-    def get_tree_from_id(qid: str, seed: WikiSeed = FamilySeed.instance(), branch_up_levels: int = 2, branch_down_levels: int = 2) -> Dict[str, any]:
+    def get_tree_from_id(
+        qid: str,
+        seed: WikiSeed = FamilySeed.instance(),
+        branch_up_levels: int = 1,
+        branch_down_levels: int = 1,
+    ) -> Dict[str, any]:
         tree = WikiTree(seed)
-        tree.grow_levels(
-            qid, branch_up_levels, branch_down_levels)
+        tree.grow_levels(qid, branch_up_levels, branch_down_levels)
         return tree.to_json()
 
     def _query_cli() -> None:
         name = input("Name to Query: ")
-        matches = list(
-            HistreeQuery.search_matching_names(name).items())
+        matches = list(HistreeQuery.search_matching_names(name).items())
 
         print("Potential matches for Query: ")
         for (i, label) in enumerate(matches):
             print(f"{i + 1}. {label[1]}")
 
-        index = int(
-            input("Choose the option you wish to query: ")) - 1
+        index = int(input("Choose the option you wish to query: ")) - 1
         qid = matches[index][0]
 
         tree_json = HistreeQuery.get_tree_from_id(qid)
