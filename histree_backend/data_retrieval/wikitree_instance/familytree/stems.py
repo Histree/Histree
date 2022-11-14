539cd2eaddd7996c14d4824696d91cb1c5f929b0
matsagad <matsagad@gmail.com>
2022-11-14 19:10:04 +0000

    HIS-56: make WikiStems use query templates

--- histree_backend/data_retrieval/wikitree_instance/familytree/stems.py
+++ histree_backend/data_retrieval/wikitree_instance/familytree/stems.py
@@ -1,53 +1,55 @@
 from typing import Dict, List
-
+from data_retrieval.query.builder import SPARQLBuilder
 from data_retrieval.wikitree.flower import WikiFlower, WikiStem
 from qwikidata.entity import WikidataItem
 from data_retrieval.wikitree_instance.familytree.property import PROPERTY_MAP
-
 
 
 class HumanStem(WikiStem):
     def __init__(self, id):
         super().__init__(id)
 
-    def parse(self, item: WikidataItem, flowers: Dict[str, WikiFlower]) -> List[WikiFlower]:
-        humans = [claim.mainsnak.datavalue.value['id'] for claim in item.get_claim_group(
-            self.id)._claims if claim.mainsnak.datavalue]
-        if not humans:
-            return []
-        return [flowers.get(human, WikiFlower(human, dict())) for human in humans]
+    def set_query_template(self, headers: Dict[str, any]) -> None:
+        self.template = (
+            SPARQLBuilder(headers)
+            .with_property(self.id, self._TEMPLATE_STR)
+            .build()
+        )
 
 
 class ChildStem(HumanStem):
     def __init__(self):
         super().__init__(PROPERTY_MAP["stems"]["child"])
 
-
-class FatherStem(HumanStem):
-    def __init__(self):
-        super().__init__(PROPERTY_MAP["stems"]["father"])
-
-
-class MotherStem(HumanStem):
-    def __init__(self):
-        super().__init__(PROPERTY_MAP["stems"]["mother"])
+    def set_query_template(self, headers: Dict[str, any]) -> None:
+        self.template = (
+            SPARQLBuilder(headers)
+            .with_any_property(
+                [
+                    PROPERTY_MAP["stems"][label]
+                    for label in ("mother", "father", "parent")
+                ],
+                self._TEMPLATE_STR,
+            )
+            .build()
+        )
 
 
 class ParentStem(WikiStem):
     def __init__(self):
         super().__init__(PROPERTY_MAP["stems"]["parent"])
 
-    def parse(self, item: WikidataItem, flowers: Dict[str, WikiFlower]) -> List[WikiFlower]:
-        return sum([parent_stem.instance().parse(item, flowers) for parent_stem in (FatherStem, MotherStem)], [])
+    def set_query_template(self, headers: Dict[str, any]) -> None:
+        self.template = (
+            SPARQLBuilder(headers)
+            .with_property(
+                PROPERTY_MAP["stems"]["child"],
+                self._TEMPLATE_STR,
+            )
+            .build()
+        )
 
 
-class SpouseStem(WikiStem):
+class SpouseStem(HumanStem):
     def __init__(self):
         super().__init__(PROPERTY_MAP["stems"]["spouse"])
-
-    def parse(self, item: WikidataItem, flowers: Dict[str, WikiFlower]) -> List[WikiFlower]:
-        spouses = [claim.mainsnak.datavalue.value['id'] for claim in item.get_claim_group(
-            self.id)._claims if claim.mainsnak.datavalue]
-        if not spouses:
-            return []
-        return [flowers.get(spouse, WikiFlower(spouse, dict())) for spouse in spouses]
