9ae9a1f9a30b7dd5b28e7b91d16400c3715a960c
Athena Yim <asy20@ic.ac.uk>
2022-11-30 14:07:49 +0000

    HIS-70: remove unused import

--- histree-frontend/src/models/index.ts
+++ histree-frontend/src/models/index.ts
@@ -1,7 +1,6 @@
 export * from "./renderContent";
 export * from "./selected";
 export * from "./graphInfo";
-export * from "./expandInfo";
 export * from "./edgeInfo";
 export * from "./filterInfo";
 export * from "./compareInfo";
