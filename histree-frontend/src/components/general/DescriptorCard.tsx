36bb4c4242fc83cad7f7fe7fa776b5f954836f17
zaki-amin <zakiamin0402@gmail.com>
2022-11-30 11:52:55 +0000

    HIS-87-add-wiki-links: remove unused expansion tab

--- histree-frontend/src/components/general/DescriptorCard.tsx
+++ histree-frontend/src/components/general/DescriptorCard.tsx
@@ -22,22 +22,6 @@ import { mapsURL } from '../../utils/utils';
 
 
 
-interface ExpandMoreProps extends IconButtonProps {
-  expand: boolean;
-}
-
-const ExpandMore = styled((props: ExpandMoreProps) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
-
-
 
 
 interface DescriptorCardProps {
